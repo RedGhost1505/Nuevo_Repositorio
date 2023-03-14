86e49e2f21bf44c77e8105387c15f8182d719e07
RedGhost1505 <joshalejandro117@gmail.com>
2023-03-14 10:09:28 -0600

    Nueva version de API y Nueva version Templates

--- API.py
+++ API.py
@@ -2,60 +2,84 @@ from flask import Flask, render_template, jsonify, abort, request
 
 app=Flask(__name__)
 
-uri='/api/'
-persona={'name':'Alex', 'matricula':'1234'}
-
 tasks=[{
+
         'id':1, 
         'name':'cocinar algo bien sabroso', 
-        'status': False
+        'check': False
         }, 
+
         {'id':2, 
          'name':'limpiar la casa', 
-         'status': False
+         'check': False
          }, 
 ]
 
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", data=persona)
-
-#API
+    return "Bienvenido a mi API de un TO-DO List"
 
 
-@app.route(uri, methods=['GET'])
+@app.route('/api/tasks', methods=['GET'])
 def getTasks():
     return jsonify({'tasks':tasks})
 
-@app.route(uri+'/<int:id>', methods=['GET'])
+
+
+@app.route('/api/tasks/<int:id>', methods=['GET'])
 def get_task(id):
-    this_task=0
-    for task in tasks:
-        if task['id']==id:
-            this_task=task
-    if this_task==0:
+    
+    this_task = [task for task in tasks if task['id']==id]
+    if len(this_task)==0:
         abort(404)
-    return jsonify({'task':this_task})
+    return jsonify({'task': this_task[0]})
+    
 
-@app.route(uri, methods =['POST'])
+
+@app.route('/api/tasks', methods =['POST'])
 def create_task():
-    if request.json:
-        task = {
-            'id': len(tasks)+1,
-            'name': request.json['name'],
-            'status': False 
-        }
-        tasks.append(task)
-        return jsonify({'tasks':tasks}), 201
-    else:
+    if not request.json:
         abort(404)
 
-@app.route(uri+'/<int:id>', methods= ['PUT'])
-def update_task(id):
-    if request.json:
-        this_task
+     
+    task = {
+        'id': len(tasks)+1,
+        'name': request.json['name'],
+        'check': False 
+    }
 
+    tasks.append(task)
+    return jsonify({'task':task}), 201
+    
+
+
+
+@app.route('/api/tasks/<int:id>', methods= ['PUT'])
+def update_task(task_id):
+    if request.json:
+        this_task = [task for task in tasks if task['id'] == task_id]
+        if len(this_task) == 0:
+            abort(404)
+        if not request.json:
+            abort(404)
+        if 'name' in request.json and type (request.json['name']) is not str:
+            abort(400)
+        if 'check' in request.json and type (request.json['name']) is not bool:
+            abort(400)
+        this_task[0]['name'] = request.json.get ('name',this_task[0]['name'])
+        this_task[0]['check']= request.json.get ('check',this_task[0]['check'])
+        return jsonify ({'task': this_task[0]}) 
+
+
+
+@app.route('/api/tasks/<int:id>', methods= ['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id']==task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
 
 
 
